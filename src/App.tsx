2ebc050571539bae46efcc09a366f98106b09032
alid didarali <aliddidarali02@gmail.com>
2021-04-09 00:15:08 +0100

    Dropdown button with filter property

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,28 @@
-import React from 'react';
+import React, {  ChangeEvent, useState } from 'react';
+import { Col, Container, Form, Row } from 'react-bootstrap';
 import './App.css';
 import EquipmentTable from './components/EquipmentTable'
 
 function App() {
+   const [type, setType] = useState("All");
+
+   const handleEquipmentSelect = (event:ChangeEvent<any>) => setType(event.target.value)
   return (
-    <div className="App">
-      <EquipmentTable />
-    </div>
+    <Container className="App">
+      <Row >
+        <Col ><h1 >Bright Bridge Council Pollution level</h1></Col>
+        <Col lg={{span: 3, offset: 2}}>
+        <Form.Control as="select" onChange={handleEquipmentSelect}>
+          <option>All</option>
+          <option>Standard</option>
+          <option>Enhanced</option>
+        </Form.Control>
+        </Col>
+      </Row>
+      <Row>
+        <EquipmentTable type={type}/>
+      </Row>
+    </Container>
   );
 }
 
--- src/components/EquipmentTable.tsx
+++ src/components/EquipmentTable.tsx
@@ -3,9 +3,17 @@ import Table from 'react-bootstrap/Table';
 import { zephyrs, colours } from '../utils';
 
 const rowColor = (NO2: number, PM25: number) => colours.find(({ value }) => value === (NO2 + PM25) / 2)?.hex
-function EquipmentTable() {
-  const bridgeBridgeOwned = zephyrs.filter(({ currentlyOwned }) => currentlyOwned)
-  const equimentSortByType = bridgeBridgeOwned.sort((a, b) => b.type.localeCompare(a.type))
+function EquipmentTable(props: { type: string; }) {
+  const bridgeBridgeOwned = zephyrs.filter(({ currentlyOwned }) => currentlyOwned);
+
+  const isAllEquipmentSelected = props.type === 'All';
+
+  const userSelectedType = isAllEquipmentSelected ? bridgeBridgeOwned
+    : bridgeBridgeOwned.filter(({ type }) => props.type === type);
+
+  const equimentSortByType = isAllEquipmentSelected ?
+    userSelectedType.sort((a, b) => b.type.localeCompare(a.type)) : userSelectedType;
+
   console.log('equ', equimentSortByType)
   return (
     <Table striped borderless hover>
@@ -20,7 +28,7 @@ function EquipmentTable() {
         </tr>
       </thead>
       <tbody>
-        {bridgeBridgeOwned.map(({ name, type, batteryPercentage, NO2, PM25 }, index) => {
+        {equimentSortByType.map(({ name, type, batteryPercentage, NO2, PM25 }, index) => {
           return (
             <tr style={{ backgroundColor: rowColor(NO2, PM25) }} key={index}>
               <td >{index + 1}</td>
