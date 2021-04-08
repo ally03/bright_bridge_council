58e4ecfc94b3960d0cd817b2d18a3817c82a8fcf
alid didarali <aliddidarali02@gmail.com>
2021-04-08 22:14:49 +0100

    Filter by bright bridge council owned

--- src/components/EquipmentTable.tsx
+++ src/components/EquipmentTable.tsx
@@ -3,34 +3,35 @@ import Table from 'react-bootstrap/Table';
 import zephyrs from '../utils';
 
 function EquipmentTable() {
-    return (
-        <Table striped bordered hover>
-        <thead>
-          <tr>
-            <th>#</th>
-            <th>Name</th>
-            <th>Type</th>
-            <th>Battery Percentage</th>
-            <th>NO2</th>
-            <th>PM25</th>
-          </tr>
-        </thead>
-        <tbody>
-         { zephyrs.map(({name, type, batteryPercentage, NO2, PM25}, index) => {
-          return( 
+  const bridgeBridgeOwned = zephyrs.filter(({ currentlyOwned }) => currentlyOwned)
+  return (
+    <Table striped bordered hover>
+      <thead>
+        <tr>
+          <th>#</th>
+          <th>Name</th>
+          <th>Type</th>
+          <th>Battery Percentage</th>
+          <th>NO2</th>
+          <th>PM25</th>
+        </tr>
+      </thead>
+      <tbody>
+        {bridgeBridgeOwned.map(({ name, type, batteryPercentage, NO2, PM25 }, index) => {
+          return (
             <tr key={index}>
-             <td>{index + 1}</td>
-             <td>{name}</td>
-             <td>{type}</td>
-             <td>{batteryPercentage}</td>
-             <td>{NO2}</td>
-             <td>{PM25}</td>
-           </tr>
-         )})}
-        </tbody>
-      </Table>
-    );
-  }
-  
-  export default EquipmentTable;
-  
+              <td>{index + 1}</td>
+              <td>{name}</td>
+              <td>{type}</td>
+              <td>{batteryPercentage}</td>
+              <td>{NO2}</td>
+              <td>{PM25}</td>
+            </tr>
+          )
+        })}
+      </tbody>
+    </Table>
+  );
+}
+
+export default EquipmentTable;
